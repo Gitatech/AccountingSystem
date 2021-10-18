713794b1eb5ad565c4430d112f8ec81ec3f26275
Alexander Maisak <sashamaisak094@gmail.com>
2021-10-19 00:22:34 +0300

    created POJO class Floor

---
+++ university-lab/src/main/java/com/university/lab/model/Floor.java
@@ -0,0 +1,36 @@
+package com.university.lab.model;
+
+import java.util.Objects;
+
+public class Floor {
+
+    private final double floorHeight;
+
+    public Floor(double floorHeight) {
+        this.floorHeight = floorHeight;
+    }
+
+    public double getFloorHeight() {
+        return floorHeight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Floor floor = (Floor) o;
+        return Double.compare(floor.floorHeight, floorHeight) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(floorHeight);
+    }
+
+    @Override
+    public String toString() {
+        return "Floor{" +
+                "floorHeight=" + floorHeight +
+                '}';
+    }
+}
