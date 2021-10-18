f87b0e5067ba6c6ece635b841b9651971d43d4e4
Fedya <ottokore@mail.ru>
2021-10-18 22:11:38 +0300

    Class house works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        house h_1 = new house();
-//        h_1.NumberOfFloors();
-        flat F = new flat();
-        F.print();
+        house h_1 = new house();
+        h_1.ResSquFlo();
     }
 }
--- src/com/company/house.java
+++ src/com/company/house.java
@@ -6,16 +6,67 @@ import static java.lang.System.out;
 public class house {
     private double totalSquare;
     private int residents;
+    private int Floors;
 
     private int flatsPerFloor; //the number of flats per floor
     private int flatNumber; // the total number of flats in the house
 
-    void NumberOfFloors(){
-        out.printf("The house consists of %d floors\n", flatNumber/flatsPerFloor);
+    void ResSquFlo(){
+        flat[] f = new flat[flatNumber];
+        for(int i = 0; i < flatNumber; i++){
+            f[i] = new flat();
+            this.residents += f[i].person;
+            this.totalSquare += f[i].square;
+        }
+        this.Floors = flatNumber/flatsPerFloor;
+        out.printf("The house has %d floors, %d residents and %f square\n", Floors, residents, totalSquare);
     }
+    public class flat {
+        private double square;
+        private int person;
+        private int size;
+
+        void print() {
+            out.printf("%d people live in %d-room flat with %f square\n", person, size, square);
+        }
+
+        flat() {
+            Scanner in = new Scanner(System.in);
+                while (size < 1 || size > 4) {
+                    try {
+                        out.println("Input the size of an apartment from 1 to 4:");
+                        this.size = in.nextInt();
+                        switch (size) {
+                            case 1 -> {
+                                //the number of people living in the apartment varies in the range of 0-3
+                                this.person = (int) (Math.random() * 10) % 4;
+                                //the area of the apartment varies in the range of 27-30 square meters
+                                this.square = (Math.random() * 100) % 4 + 27;
+                            }
+                            case 2 -> {
+                                this.person = (int) (Math.random() * 10) % 5;
+                                this.square = (Math.random() * 100) % 6 + 45;
+                            }
+                            case 3 -> {
+                                this.person = (int) (Math.random() * 10) % 6;
+                                this.square = (Math.random() * 100) % 6 + 60;
+                            }
+                            case 4 -> {
+                                this.person = (int) (Math.random() * 10) % 7;
+                                this.square = (Math.random() * 100) % 6 + 75;
+                            }
+                            default -> throw new Exception("It's not the flat type.");
+                        }
+                    } catch (Exception ex) {
+                        out.println(ex.getMessage() + " Input new flat type from 1 to 4:");
+                        this.size = in.nextInt();
+                    }
+                }
+            }
+        }
 
     house() {
-        try(Scanner in = new Scanner(System.in)) {
+        Scanner in = new Scanner(System.in);
             while(flatsPerFloor <= 0){
                 try {
                     out.println("Input the number of apartments per floor:");
@@ -44,5 +95,3 @@ public class house {
             }
         }
     }
-
-}
