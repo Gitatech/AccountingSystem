624f35f9b161ba38d1de9512198d73838ea976dd
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2021-12-10 13:41:19 +0300

    added FlatService and modified HouseService

--- src/main/java/com/prokopchyk/service/HouseService.java
+++ src/main/java/com/prokopchyk/service/HouseService.java
@@ -1,47 +1,76 @@
 package com.prokopchyk.service;
 
+import com.prokopchyk.building.Flat;
+import com.prokopchyk.building.Ground;
 import com.prokopchyk.building.House;
-import com.prokopchyk.building.flatComparator.FlatAreaCompare;
-import com.prokopchyk.building.flatComparator.FlatPersonsCompare;
 import com.prokopchyk.building.houseComparator.AreaCompare;
 import com.prokopchyk.building.houseComparator.PersonsCompare;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class HouseService  {
-
-    public static double getHouseArea(House house) {
+    private static  HouseService houseService;
+    private HouseService(){};
+    public synchronized static HouseService getHouseService(){
+        if(houseService == null)
+            houseService = new HouseService();
+        return houseService;
+    }
+    public  double getHouseArea(House house) {
         int sqrt = 0;
-        for(int i = 0; i< house.getNumberOfGroundsInHouse(); i++)
-        {
-            sqrt += house.getGround(i).getGroundArea();
+        for(int i = 0; i < house.getNumberOfGrounds(); i++){
+            for(int j = 0; j < house.getGrounds().get(0).getFlatsOnGround(); j++) {
+            sqrt += house.getGround(i).getFlat(j).getSqrt();
+            }
         }
         return sqrt;
     }
 
-    public static int getNumberOfHuman(House house){  // возвращает общее число жильцов
+    public int getNumberOfHuman(House house){  // возвращает общее число жильцов
         int kol = 0;
-        for(int i = 0; i< house.getNumberOfGroundsInHouse(); i++)
-        {
-            kol += house.getGround(i).getManInGround();
+        for(int i = 0; i < house.getNumberOfGrounds(); i++){
+            for(int j = 0; j < house.getGrounds().get(0).getFlatsOnGround(); j++) {
+                kol += house.getGround(i).getFlat(j).getNumberOfHuman();
+            }
         }
         return kol;
     }
-
-    public static List<Integer> compareFlats(House house,int num1,int num2){
-        List<Integer> answer = new ArrayList<Integer>();
-        answer.add(new FlatAreaCompare().compare(house.getGroundByFlatNumber(num1).getFlatByNumber(num1)
-                ,house.getGroundByFlatNumber(num2).getFlatByNumber(num2)));
-        answer.add(new FlatPersonsCompare().compare(house.getGroundByFlatNumber(num1).getFlatByNumber(num1)
-                ,house.getGroundByFlatNumber(num2).getFlatByNumber(num2)));
-        return answer;
-    }
-
-    public static List<Integer> compareHouses(House house1,House house2){
+    public  List<Integer> compareHouses(House house1,House house2){
         List<Integer> answer = new ArrayList<Integer>();
         answer.add(new AreaCompare().compare(house1,house2));
         answer.add(new PersonsCompare().compare(house2,house2));
         return  answer;
+    }
+    public Flat getFlatByNumber(House house, int i){
+        int gr = i / (house.getGrounds().get(0).getFlatsOnGround());
+        int fl = i % (house.getGrounds().size());
+        return house.getGrounds().get(gr).getFlat(fl);
+    }
+
+    public int getNumberOfFlats(House house){
+        return house.getNumberOfGrounds() * house.getGrounds().get(0).getFlatsOnGround();
+    }
+
+    public void initPersons(House house){
+        for(int i = 0; i < house.getNumberOfGrounds(); i++){
+            for(int j = 0; j < house.getGrounds().get(0).getFlatsOnGround(); j++){
+                System.out.println("Enter number of persons in "+house.getGrounds().get(i).getFlat(j).getNumber()+" flat");
+                Scanner in = new Scanner(System.in);
+                int persons =  in.nextInt();
+                house.getGrounds().get(i).getFlat(j).setNumberOfHuman(persons);
+            }
+        }
+    }
+
+    public void initPersonsRandom(House house){
+        for(int i = 0; i < house.getNumberOfGrounds(); i++){
+            for(int j = 0; j < house.getGrounds().get(0).getFlatsOnGround(); j++){
+                int randPersons = (int) (Math.random()*5);
+                house.getGrounds().get(i).getFlat(j).setNumberOfHuman(randPersons);
+            }
+        }
     }
 }
